7d490be3e962bb4b4bcbccce359217786305d1d4
jimbiscuit <julienchandelle@gmail.com>
2024-03-12 14:49:51 +0100

    URB-3005 fix import uid and @id (#2)
    ---------
    
    Co-authored-by: Martin Peeters <martin.peeters@affinitic.be>

--- src/urban/schedule/utils.py
+++ src/urban/schedule/utils.py
@@ -22,6 +22,8 @@ def remove_uid(data):
     for item in data:
         if "UID" in item:
             del item["UID"]
+        if "UID" in item["parent"]:
+            del item["parent"]["UID"]
         new_data.append(item)
 
     return new_data
@@ -31,11 +33,40 @@ def remove_none(data):
     return [{k: v for k, v in item.items() if v is not None} for item in data]
 
 
+def fix_id(id):
+    path = id.split("portal_urban")[1]
+    root_site = getSite()
+    return os.path.normpath(
+        os.path.join(
+            "/".join(root_site.getPhysicalPath()), "portal_urban", path.lstrip("/")
+        )
+    )
+
+
+def fix_all_ids(data):
+    new_data = []
+
+    for item in data:
+        item["@id"] = fix_id(item["@id"])
+        item["parent"]["@id"] = fix_id(item["parent"]["@id"])
+        new_data.append(item)
+
+    return new_data
+
+
 def import_json_config(
     json_path,
     context,
 ):
+    """
+    This function is used to import a json file (exported with c.exportimport)
 
+    :param json_path: Path to the json to be imported
+    :type json_path: String
+    :param context: Path to or object of the context where the json will be imported
+    :type context: String or plone object
+    :raises ValueError: Raise if the json doesn't exist
+    """
     if not os.path.isfile(json_path):
         raise ValueError("{} does not exist".format(json_path))
 
@@ -62,6 +93,7 @@ def import_json_config(
 
     data = remove_uid(data)
     data = remove_none(data)
+    data = fix_all_ids(data)
 
     import_content.start()
     import_content.do_import(data)
@@ -73,6 +105,16 @@ def import_all_config(
     base_context_path="portal_urban",
     config_type="schedule",
 ):
+    """
+    Function used to import all json inside a folder
+
+    :param base_json_path: Root folder whre to find json, defaults to "./profiles/config"
+    :type base_json_path: String, optional
+    :param base_context_path: Path to or object of the context where the json will be imported, defaults to "portal_urban"
+    :type base_context_path: String or plone object, optional
+    :param config_type: config folder whre to import, defaults to "eventconfigs"
+    :type config_type: String, optional
+    """
     directory_path = os.path.dirname(os.path.realpath(__file__))
 
     licences_types = os.walk(
@@ -86,12 +128,16 @@ def import_all_config(
             continue
         for file in files:
             json_path = os.path.join(root, file)
-            licence_type = root.split('/')[-1]
+            licence_type = root.split("/")[-1]
             context_plone = os.path.normpath(
-                os.path.join(root_site.id, base_context_path, licence_type, config_type)
+                os.path.join(
+                    "/".join(root_site.getPhysicalPath()),
+                    base_context_path,
+                    licence_type,
+                    config_type,
+                )
             )
             import_json_config(
                 json_path=json_path,
                 context=context_plone,
-                existing_content=ExistingContent.SKIP,
             )
