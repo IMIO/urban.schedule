5fe7b1f89ff945a48f0f94a49ce0e3fc85b00c3c
Martin Peeters <martin.peeters@affinitic.be>
2024-03-25 13:07:14 +0100

    URB-3005: Handle specific configuration for Liege and Urban classic

--- src/urban/schedule/setuphandlers.py
+++ src/urban/schedule/setuphandlers.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+from urban.schedule.utils import import_all_config
+
+import os
 
 
 @implementer(INonInstallable)
@@ -14,7 +18,14 @@ class HiddenProfiles(object):
 
 def post_install(context):
     """Post install script"""
-    # Do something at the end of the installation of this package.
+    configs = os.environ.get("URBAN_SCHEDULE_CONFIGS", "default,standard")
+    configs = configs.split(",")
+
+    for config in configs:
+        import_all_config(
+            base_json_path="./profiles/config/{0}".format(config),
+            base_context_path="portal_urban",
+        )
 
 
 def uninstall(context):
