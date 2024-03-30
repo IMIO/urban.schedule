0f56c4f5b30e12be7236f4164e32a511e3a2a091
Martin Peeters <martin.peeters@affinitic.be>
2024-03-30 22:32:39 +0100

    URB-3005: Add an upgrade step to update reception tasks

--- src/urban/schedule/upgrades.py
+++ src/urban/schedule/upgrades.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from urban.schedule.utils import import_all_config
-from plone import api
+from urban.schedule import utils
+
+import logging
+
+logger = logging.getLogger("urban.events: migrations")
 
 
 def import_schedule_config(context):
-    import_all_config()
+    utils.import_all_config()
+
+
+def update_reception(context):
+    logger.info("starting : Update reception tasks")
+    if "standard" in utils.get_configs():
+        utils.import_all_config(
+            base_json_path="./profiles/config/standard",
+            handle_existing_content=utils.ExistingContent.UPDATE,
+            match_filename="reception.json",
+        )
+    else:
+        logger.info("nothing to upgrade")
+    logger.info("upgrade done!")
